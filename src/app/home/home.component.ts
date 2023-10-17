c48a59cadbf94e0f3283b62d944b262e34763805
Andrew Ramires May <andrewc3po@gmail.com>
2023-10-17 11:11:38 -0400

    Correção

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ConfigService } from '../core/services/config.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor(private router: Router) { }
-
-  ngOnInit(): void {
-    console.log('HomeComponent INIT');
+  constructor(private config : ConfigService) { }
+  
+  public getUrlServer() {
+    return this.config.getHost();
   }
-
 
 }
