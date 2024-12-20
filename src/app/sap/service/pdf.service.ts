daaa0722ca099ca97daefa70969b4b442d0c65f2
Pedr0Colombo <pedro.colomboprof1@gmail.com>
2024-12-20 14:42:48 -0400

    pdf v3

--- src/app/sap/service/pdf.service.ts
+++ src/app/sap/service/pdf.service.ts
@@ -3,19 +3,19 @@ import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PdfService {
   constructor() {}
 
   gerarPdfDoElemento(element: HTMLElement, fileName: string) {
-    html2canvas(element, { scale: 3 }).then((canvas: HTMLCanvasElement) => {
+    html2canvas(element, { scale: 2 }).then((canvas) => {
       const imgData = canvas.toDataURL('image/png');
       const pdf = new jsPDF('p', 'mm', 'a4');
-      const pdfWidth = pdf.internal.pageSize.getWidth();
-      const pdfHeight = pdf.internal.pageSize.getHeight();
-    
-      pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+      const pageWidth = pdf.internal.pageSize.getWidth();
+      const pageHeight = (canvas.height * pageWidth) / canvas.width;
+
+      pdf.addImage(imgData, 'PNG', 0, 0, pageWidth, pageHeight);
       pdf.save(fileName);
     });
   }
