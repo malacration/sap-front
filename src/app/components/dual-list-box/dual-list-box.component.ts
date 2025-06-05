aa79339a2d1192b97f5daae70fdc4ac466b82ed7
PedroColomb0 <pedrocolombodev@gmail.com>
2025-06-05 16:46:20 -0400

    ajustando a dual list

--- src/app/components/dual-list-box/dual-list-box.component.ts
+++ src/app/components/dual-list-box/dual-list-box.component.ts
@@ -13,6 +13,17 @@ export class DualListBoxComponent {
 
   searchTermAvailable = '';
   searchTermSelected = '';
+  carregamentoPorPedido: boolean = false;
+
+  // Função de ordenação por DocNum e ItemCode
+  private sortItems(items: PedidoVenda[]): PedidoVenda[] {
+    return items.sort((a, b) => {
+      if (a.DocNum !== b.DocNum) {
+        return a.DocNum - b.DocNum; // Ordena por DocNum ascendente
+      }
+      return a.ItemCode.localeCompare(b.ItemCode); // Dentro do mesmo DocNum, ordena por ItemCode
+    });
+  }
 
   // Group available items by DocNum
   get groupedAvailableItems(): { docNum: number, items: PedidoVenda[] }[] {
@@ -25,11 +36,13 @@ export class DualListBoxComponent {
       return acc;
     }, {} as { [key: number]: { docNum: number, items: PedidoVenda[] } });
 
-    return Object.values(grouped).filter(group =>
-      group.items.some(item =>
-        `${item.CardCode} - ${item.DocNum}`.toLowerCase().includes(this.searchTermAvailable.toLowerCase())
+    return Object.values(grouped)
+      .filter(group =>
+        group.items.some(item =>
+          `${item.CardCode} - ${item.DocNum}`.toLowerCase().includes(this.searchTermAvailable.toLowerCase())
+        )
       )
-    );
+      .sort((a, b) => a.docNum - b.docNum); // Ordena os grupos por DocNum
   }
 
   // Group selected items by DocNum
@@ -43,33 +56,51 @@ export class DualListBoxComponent {
       return acc;
     }, {} as { [key: number]: { docNum: number, items: PedidoVenda[] } });
 
-    return Object.values(grouped).filter(group =>
-      group.items.some(item =>
-        `${item.CardCode} - ${item.DocNum}`.toLowerCase().includes(this.searchTermSelected.toLowerCase())
+    return Object.values(grouped)
+      .filter(group =>
+        group.items.some(item =>
+          `${item.CardCode} - ${item.DocNum}`.toLowerCase().includes(this.searchTermSelected.toLowerCase())
+        )
       )
-    );
+      .sort((a, b) => a.docNum - b.docNum); // Ordena os grupos por DocNum
+  }
+
+  toggleCarregamentoPorPedido(): void {
+    this.carregamentoPorPedido = !this.carregamentoPorPedido;
   }
 
   selectItem(item: PedidoVenda): void {
-    this.selectedItems = [...(this.selectedItems || []), item];
-    this.availableItems = (this.availableItems || []).filter(i => i.DocNum !== item.DocNum);
+    if (this.carregamentoPorPedido) {
+      const itemsToMove = this.availableItems.filter(i => i.DocNum === item.DocNum);
+      this.selectedItems = this.sortItems([...(this.selectedItems || []), ...itemsToMove]);
+      this.availableItems = this.sortItems((this.availableItems || []).filter(i => i.DocNum !== item.DocNum));
+    } else {
+      this.selectedItems = this.sortItems([...(this.selectedItems || []), item]);
+      this.availableItems = this.sortItems((this.availableItems || []).filter(i => i !== item));
+    }
     this.selectedItemsChange.emit(this.selectedItems);
   }
 
   removeItem(item: PedidoVenda): void {
-    this.availableItems = [...(this.availableItems || []), item];
-    this.selectedItems = (this.selectedItems || []).filter(i => i.DocNum !== item.DocNum);
+    if (this.carregamentoPorPedido) {
+      const itemsToRemove = this.selectedItems.filter(i => i.DocNum === item.DocNum);
+      this.availableItems = this.sortItems([...(this.availableItems || []), ...itemsToRemove]);
+      this.selectedItems = this.sortItems((this.selectedItems || []).filter(i => i.DocNum !== item.DocNum));
+    } else {
+      this.availableItems = this.sortItems([...(this.availableItems || []), item]);
+      this.selectedItems = this.sortItems((this.selectedItems || []).filter(i => i !== item));
+    }
     this.selectedItemsChange.emit(this.selectedItems);
   }
 
   selectAll(): void {
-    this.selectedItems = [...(this.selectedItems || []), ...(this.availableItems || [])];
+    this.selectedItems = this.sortItems([...(this.selectedItems || []), ...(this.availableItems || [])]);
     this.availableItems = [];
     this.selectedItemsChange.emit(this.selectedItems);
   }
 
   removeAll(): void {
-    this.availableItems = [...(this.availableItems || []), ...(this.selectedItems || [])];
+    this.availableItems = this.sortItems([...(this.availableItems || []), ...(this.selectedItems || [])]);
     this.selectedItems = [];
     this.selectedItemsChange.emit(this.selectedItems);
   }
