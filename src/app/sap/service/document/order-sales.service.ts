f04805457c070cfb6143ac307f1dd43e07f574a9
PedroColomb0 <pedrocolombodev@gmail.com>
2025-09-11 11:09:36 -0400

    adicionando adicionar nota fiscal procurar ordem de carregamento, colocar ordem de carregamento

--- src/app/sap/service/document/order-sales.service.ts
+++ src/app/sap/service/document/order-sales.service.ts
@@ -1,9 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { ConfigService } from '../../../core/services/config.service';
 import { DocumentAngularSave } from './document-angular-save';
 import { PedidoVenda } from '../../components/document/documento.statement.component';
+import { NextLink } from '../../model/next-link';
 
 @Injectable({
   providedIn: 'root'
@@ -19,4 +20,61 @@ export class OrderSalesService  implements DocumentAngularSave{
   save(body : PedidoVenda) : Observable<any>{
     return this.hppCliente.post<any>(this.url+"/angular",body)
   }
+
+    search(dataInicial: string, dataFinal: string, filial: string, localidade: string): Observable<NextLink<PedidoVenda>> {
+    let params = new HttpParams()
+      .set('filial', filial.toString())
+      .set('localidade', localidade.toString());
+
+    if (dataInicial) {
+      params = params.set('dataInicial', dataInicial);
+    }
+
+    if (dataFinal) {
+      params = params.set('dataFinal', dataFinal);
+    }
+
+    return this.hppCliente
+      .get<NextLink<PedidoVenda>>(`${this.url}/search`, { params })
+      .pipe(
+        map((response) => {
+          response.content = response.content.map((item) => Object.assign(new PedidoVenda(), item));
+          return response;
+        })
+      );
+  }
+
+  searchAll(nextLink: string): Observable<NextLink<PedidoVenda>> {
+    return this.hppCliente
+      .post<NextLink<PedidoVenda>>(`${this.url}/searchAll`, nextLink)
+      .pipe(
+        map((response) => {
+          response.content = response.content.map((item) => Object.assign(new PedidoVenda(), item));
+          return response;
+        })
+      );
+  }
+
+  updateOrdemCarregamento(pedidoId: string, ordemCarregamentoId: number): Observable<any> {
+    return this.hppCliente.get(
+      `${this.url}/pedido/all/${pedidoId}/${ordemCarregamentoId}/order-id`
+    );
+  }
+
+  getPedidosBy(ordemId: number): Observable<PedidoVenda[]> {
+    return this.hppCliente
+      .get<NextLink<PedidoVenda>>(`${this.url}/search2`, {
+        params: { U_Ordem_Carregamento: ordemId.toString() }
+      })
+      .pipe(
+        map((response: NextLink<PedidoVenda>) => {
+          // A resposta tem propriedade 'content' com o array
+          return response.content.map((item: any) => Object.assign(new PedidoVenda(), item));
+        }),
+        catchError((error) => {
+          console.error('Erro no getPedidosBy:', error);
+          return of([]); // Retorna array vazio em caso de erro
+        })
+      );
+    }
 }
