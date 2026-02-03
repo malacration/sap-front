719d5b6bef4058fa04762a32354b160c2ee606a0
Andrew Ramires May <andrewc3po@gmail.com>
2026-02-03 14:16:47 -0400

    correção

--- src/app/modulos/calculadora-preco-venda/components/formacao/formacao.component.ts
+++ src/app/modulos/calculadora-preco-venda/components/formacao/formacao.component.ts
@@ -237,6 +237,10 @@ export class FormacaoPrecoStatementComponent implements OnInit, OnChanges {
     // )
   }
 
+  solicitarProdutos($event){
+
+  }
+
   //TODO isso seria de um tableservice talvez
   removeKeys(obj){
     for (const key in obj) {
